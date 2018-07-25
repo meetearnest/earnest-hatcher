3bbbe2e8d24296ed30c8b34464d0afbdeefb6248
Steven Uray <steven.uray@earnest.com>
2018-07-25 16:09:59 -0700

    Initial bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -euo pipefail
+
+function setup_kyrios {
+	mkdir ~/.kyrios
+	cp -v ./assets/earnest-kyrios-profile.yaml ~/.kyrios/profile.yaml
+
+}
+
+function display_earnest_logo {
+	echo
+	echo -e "                                                                     _..._"
+	echo -e "                                                                    d88888b."
+	echo -e "                                                                   d8\"   \"8b"
+	echo -e "    _..._       _...._    .... _.. .... _.._        _..._       _..Y8     88"
+	echo -e "  .8888888.    o8888888.  |88|o888 |88|d88888.    .8888888.   .888888>  .d88..."
+	echo -e " d88\"   \"88b  d88\"   \"88. |888P\"\"\" |88P\"''\"Y88.  d88\"   \"88b |88\"   \"   8888888"
+	echo -e ".88|.....|88.  '\"  ..d88| |88Y     |88|    |88| .88|.....|88.|88o.._     |88|"
+	echo -e "|88888888888|  .od888888| |88|     |88|    |88| |88888888888| \"Y88888b.  |88|"
+	echo -e "'88|\"\"\"\"\"\"\"\"' d8P\"\"\"  88| |88|     |88|    |88| '88|\"\"\"\"\"\"\"\"'  .  '\"Y88. |88|"
+	echo -e " Y8b     .8b.|88.    .88| |88|     |88|    |88|  Y8b     .8b.<88.   .88' |88"
+	echo -e "  \"88bood88\"  \"88boodP88| |88|     |88|    |88|   \"88bood88\"  \"88bod88\"  'Y8888"
+	echo -e "    '\"\"\"'       '\"\"\"' \"\"' '\"\"'     '\"\"'    '\"\"'     '\"\"\"'       '\"\"\"'      '\"\"\""
+	echo
+	echo
+	echo
+	sleep 2	
+}
+
+function display_startup_message {
+	echo "Bootstrapping with Kyrios..."
+}
+
+display_earnest_logo
+display_startup_message
+setup_kyrios
