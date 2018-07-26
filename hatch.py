8aa8555ffc9950a1c1ed8ea3b6e7c34eede8c449
Steven Uray <steven.uray@earnest.com>
2018-07-26 13:07:59 -0700

    Fixing target folder issue for git_clone() in hatch.py

--- hatch.py
+++ hatch.py
@@ -7,15 +7,15 @@ git_folder = '~/git/'
 setup_user_github_username = 'earntech-setup-git'
 
 def install_aws_sts_token_generator():	
-	git_clone(aws_sts_token_generator_url)
+	git_clone(aws_sts_token_generator_url, git_folder + 'aws-sts-token-generator')
 	print('install aws sts token generator here')
 
 def install_pgauth():	
-	git_clone(pgauth_url)
+	git_clone(pgauth_url, git_folder + 'pgauth')
 	print('install pgauth here')
 
-def git_clone(target_url):
-	os.system('git clone ' + target_url + ' ' + git_folder)
+def git_clone(target_url, folder):
+	os.system('git clone ' + target_url + ' ' + folder)
 
 def main(args):
 	install_aws_sts_token_generator()
