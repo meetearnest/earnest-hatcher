56520949250530b2511fdd8509e1ceebf1939b1a
Jack Anderson <jack.anderson@earnest.com>
2019-04-11 09:53:46 -0600

    clone aws-sts repo into git_folder, add .gitignore

--- hatch.py
+++ hatch.py
@@ -10,7 +10,8 @@ git_folder = '~/git/'
 def git_clone_repos(folder):
 	for git_repo in git_repos:
 		print(git_repo)
-		git_clone_if_not_already_cloned(git_repo, git_repos[git_repo])
+		target_folder = folder + git_repos[git_repo]
+		git_clone_if_not_already_cloned(git_repo, target_folder)
 
 def git_clone(target_url, folder):
 	os.system('git clone ' + target_url + ' ' + folder)
