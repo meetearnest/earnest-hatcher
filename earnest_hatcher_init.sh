053ccbedde4d7df7e621d901f3177cbd995bc49d
Steven Uray <steven.uray@earnest.com>
2019-04-19 11:43:42 -0700

    Upgrades to earnest_hatcher_init.sh for smoother install of pygo system.

--- earnest_hatcher_init.sh
+++ earnest_hatcher_init.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-GIT_DIRECTORY=git
+GIT_DIRECTORY=${HOME}/git
 
-cd ~ 
+cd ~
+echo "Attempting to install xcode-select..."
+xcode-select --install
+echo "Attempting to install Mac OS SDK Headers (needed for pyenv later)"
+sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target
 mkdir ${GIT_DIRECTORY}
 cd ${GIT_DIRECTORY}
 git clone https://github.com/meetearnest/earnest-hatcher.git
