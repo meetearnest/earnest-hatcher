f736e2b2b2b7a720b9c6ae77901558b479be9a77
Steven Uray <steven.uray@earnest.com>
2019-04-19 11:46:30 -0700

    Fix target bug in earnest_hatcher_init

--- earnest_hatcher_init.sh
+++ earnest_hatcher_init.sh
@@ -6,7 +6,7 @@ cd ~
 echo "Attempting to install xcode-select..."
 xcode-select --install
 echo "Attempting to install Mac OS SDK Headers (needed for pyenv later)"
-sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target
+sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 mkdir ${GIT_DIRECTORY}
 cd ${GIT_DIRECTORY}
 git clone https://github.com/meetearnest/earnest-hatcher.git
